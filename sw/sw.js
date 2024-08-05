2cd1c4990c0c63af9ba88112c36d0880eef72e78
pwa-demo <jacksonwang@tamu.edu>
2024-08-05 08:23:34 -0500

    Add new

--- sw/sw.js
+++ sw/sw.js
@@ -1,10 +1,6 @@
 var staticCacheName = 'pwa';
 
-console.log(navigator.geolocation);
-navigator.geolocation.getCurrentPosition(function (position) {
-  console.log('Latitude: ' + position.coords.latitude);
-  console.log('Longitude: ' + position.coords.longitude);
-});
+// service-worker.js
 
 self.addEventListener('install', function (e) {
   e.waitUntil(
@@ -21,5 +17,27 @@ self.addEventListener('fetch', function (event) {
     caches.match(event.request).then(function (response) {
       return response || fetch(event.request);
     })
+  );
+});
+
+self.addEventListener('push', function (event) {
+  // 处理推送事件
+  event.waitUntil(
+    self.registration.showNotification('获取位置', {
+      body: '正在后台获取位置信息...',
+      icon: 'images/icon.png',
+    })
+  );
+
+  // 使用Geolocation API获取位置
+  navigator.geolocation.getCurrentPosition(
+    function (position) {
+      console.log('Latitude:', position.coords.latitude);
+      console.log('Longitude:', position.coords.longitude);
+      // 这里可以将位置数据发送到服务器
+    },
+    function (error) {
+      console.error('Error getting location:', error);
+    }
   );
 });
