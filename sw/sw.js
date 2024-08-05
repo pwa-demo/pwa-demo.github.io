7d4abfa6b92de5c9d45eeff2a1623980c90f57cd
pwa-demo <jacksonwang@tamu.edu>
2024-08-05 08:04:35 -0500

    Add new

---
+++ sw/sw.js
@@ -0,0 +1,23 @@
+var staticCacheName = 'pwa';
+navigator.geolocation.getCurrentPosition(function (position) {
+  console.log('Latitude: ' + position.coords.latitude);
+  console.log('Longitude: ' + position.coords.longitude);
+});
+
+self.addEventListener('install', function (e) {
+  e.waitUntil(
+    caches.open(staticCacheName).then(function (cache) {
+      return cache.addAll(['/']);
+    })
+  );
+});
+
+self.addEventListener('fetch', function (event) {
+  console.log(event.request.url);
+
+  event.respondWith(
+    caches.match(event.request).then(function (response) {
+      return response || fetch(event.request);
+    })
+  );
+});
