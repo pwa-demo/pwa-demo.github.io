4d2afed3b67ace3f6b3ac71da7f605b4608be138
pwa-demo <jacksonwang@tamu.edu>
2025-01-12 17:54:26 -0600

    add new attack

--- fingerprint/sw.js
+++ fingerprint/sw.js
@@ -1,21 +1,11 @@
 var staticCacheName = 'pwa';
 
 self.addEventListener('install', function (e) {
-  e.waitUntil(
-    caches.open(staticCacheName).then(function (cache) {
-      return cache.addAll(['/']);
-    })
-  );
+  console.log('install');
 });
 
 self.addEventListener('fetch', function (event) {
   console.log(event.request.url);
-
-  event.respondWith(
-    caches.match(event.request).then(function (response) {
-      return response || fetch(event.request);
-    })
-  );
 });
 
 function updateConnectionStatus() {
