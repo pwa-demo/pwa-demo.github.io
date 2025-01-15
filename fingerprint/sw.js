9b87591fbedc77b22fb38da35009a4efbef98310
pwa-demo <jacksonwang@tamu.edu>
2025-01-14 20:40:09 -0600

    add new attack

--- fingerprint/sw.js
+++ fingerprint/sw.js
@@ -8,34 +8,67 @@ self.addEventListener('fetch', function (event) {
   console.log(event.request.url);
 });
 
-function updateConnectionStatus() {
-  console.log(
-    `Connection type changed from ${type} to ${navigator.connection.effectiveType}`
-  );
-  console.log('==== Network Connection Information ====');
-  console.log(
-    `Effective Connection Type: ${navigator.connection.effectiveType}`
-  );
-  console.log(
-    `Downlink (estimated bandwidth in Mbps): ${navigator.connection.downlink}`
-  );
-  console.log(`RTT (Round-trip time in ms): ${navigator.connection.rtt}`);
-  console.log(`Save Data Mode Enabled: ${navigator.connection.saveData}`);
-  console.log(
-    `Max Downlink (max theoretical bandwidth): ${navigator.connection.downlinkMax}`
-  );
-  console.log(`=== Connection Type (deprecated) ===`);
-  console.log(`Type: ${navigator.connection.type || 'Unavailable'}`);
-  console.log('======================================');
-
-  type = navigator.connection.effectiveType;
+async function checkPermission(name) {
+  try {
+    const permissionStatus = await navigator.permissions.query({ name });
+    return permissionStatus.state; // Returns 'prompt', 'granted', or 'denied' if supported
+  } catch (error) {
+    if (error instanceof TypeError) {
+      return 'no value'; // Represents lack of support
+    } else {
+      return 'error';
+    }
+  }
 }
 
-let type = navigator.connection.effectiveType;
-navigator.connection.addEventListener('change', updateConnectionStatus);
+// Begin detection logic
+const accelerometer = await checkPermission('accelerometer');
+const topLevelstorage = await checkKeyboardLockPermission();
 
-// Initial log to capture the connection status at page load
-updateConnectionStatus();
+if (
+  topLevelstorage !== 'no value' &&
+  topLevelstorage !== 'error' &&
+  topLevelstorage !== 'denied'
+) {
+  console.log('Desktop');
+} else if (accelerometer === 'no value' || accelerometer === 'error') {
+  const midi = await checkPermission('midi');
+  const storageAccess = await checkPermission('storage-access');
 
-// every 10 seconds, log the connection status
-setInterval(updateConnectionStatus, 5000);
+  if (
+    midi === 'no value' ||
+    midi === 'error' ||
+    storageAccess === 'no value' ||
+    storageAccess === 'error' ||
+    storageAccess !== 'prompt'
+  ) {
+    console.log('iOS');
+  } else {
+    console.log('Android Firefox');
+  }
+} else {
+  const paymentHandler = await checkPermission('payment-handler');
+  if (paymentHandler === 'no value') {
+    console.log('Firefox on Android');
+  } else {
+    const nfc = await checkPermission('nfc');
+    const storageAccess = await checkPermission('storage-access');
+    if (nfc === 'no value') {
+      console.log('Brave on Android');
+    } else if (storageAccess === 'denied') {
+      console.log('Opera on Android');
+    } else {
+      const backgroundSync = await checkPermission('background-fetch');
+      const periodicBackgroundSync = await checkPermission(
+        'periodic-background-sync'
+      );
+      if (backgroundSync === 'denied' && periodicBackgroundSync === 'denied') {
+        console.log('Samsung Internet on Android');
+      } else if (periodicBackgroundSync === 'denied') {
+        console.log('Edge on Android');
+      } else {
+        console.log('Chrome on Android');
+      }
+    }
+  }
+}
